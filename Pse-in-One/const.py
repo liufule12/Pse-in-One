91167a65e3377b791250d04466ac4727d059c74a
liufule12 <liufule12@gmail.com>
2015-01-04 11:28:45 +0800

    add const file

---
+++ Pse-in-One/const.py
@@ -0,0 +1,18 @@
+__author__ = 'Fule Liu'
+
+
+methods_all = ['Kmer', 'RevKmer', 'PseKNC',
+               'PseDNC', 'PC-PseDNC-General', 'SC-PseDNC-General', 'PC-PseTNC-General', 'SC-PseTNC-General',
+               'PC-PseAAC', 'PC-PseAAC-General', 'SC-PseAAC', 'SC-PseAAC-General',
+               'DAC', 'DCC', 'DACC', 'TAC', 'TCC', 'TACC', 'AC', 'CC', 'ACC']
+
+k_2_DNA_methods = ['PseDNC', 'PC-PseDNC-General', 'SC-PseDNC-General']
+k_3_DNA_methods = ['PC-PseTNC-General', 'SC-PseTNC-General']
+
+theta_1_methods = ['PseDNC', 'PC-PseDNC-General', 'PC-PseTNC-General', 'PC-PseAAC', 'PC-PseAAC-General']
+theta_2_methods = ['SC-PseDNC-General', 'SC-PseTNC-General', 'SC-PseAAC', 'SC-PseAAC-General']
+
+di_inds_6_DNA = ['Rise', 'Roll', 'Shift', 'Slide', 'Tilt', 'Twist']
+tri_inds_RNA = ['Twist (RNA)', 'Tilt (RNA)', 'Roll (RNA)', 'Rise (RNA)', 'Slide (RNA)', 'Shift (RNA)',
+                'Stacking energy (RNA)', 'Enthalpy (RNA)1', 'Entropy (RNA)', 'Free energy (RNA)', 'Hydrophilicity (RNA)']
+inds_3_Protein = ['Hydrophobicity', 'Hydrophilicity', 'Mass']
