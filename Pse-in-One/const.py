a5c1b4cfdeac5f108d900f671d15157949e270bc
liufule12 <liufule12@gmail.com>
2015-01-04 16:44:05 +0800

    rename const to upper.

--- Pse-in-One/const.py
+++ Pse-in-One/const.py
@@ -1,22 +1,31 @@
 __author__ = 'Fule Liu'
 
 
-methods_all = ['Kmer', 'RevKmer', 'PseKNC',
+METHODS_ALL = ['Kmer', 'RevKmer', 'PseKNC',
                'PseDNC', 'PC-PseDNC-General', 'SC-PseDNC-General', 'PC-PseTNC-General', 'SC-PseTNC-General',
                'PC-PseAAC', 'PC-PseAAC-General', 'SC-PseAAC', 'SC-PseAAC-General',
                'DAC', 'DCC', 'DACC', 'TAC', 'TCC', 'TACC', 'AC', 'CC', 'ACC']
+METHODS_DNA = ['Kmer', 'RevKmer', 'PseKNC', 'PseDNC',
+               'PC-PseDNC-General', 'SC-PseDNC-General', 'PC-PseTNC-General', 'SC-PseTNC-General',
+               'DAC', 'DCC', 'DACC', 'TAC', 'TCC', 'TACC']
+METHODS_RNA = ['Kmer', 'PC-PseDNC-General', 'SC-PseDNC-General', 'DAC', 'DCC', 'DACC']
+METHODS_PROTEIN = ['Kmer', 'PC-PseAAC', 'PC-PseAAC-General', 'SC-PseAAC', 'SC-PseAAC-General', 'AC', 'CC', 'ACC']
 
-k_2_DNA_methods = ['PseDNC', 'PC-PseDNC-General', 'SC-PseDNC-General']
-k_3_DNA_methods = ['PC-PseTNC-General', 'SC-PseTNC-General']
+METHODS_AC = ['DAC', 'TAC', 'AC']
+METHODS_CC = ['DCC', 'TCC', 'CC']
+METHODS_ACC = ['DACC', 'TACC', 'ACC']
 
-theta_1_methods = ['PseDNC', 'PC-PseDNC-General', 'PC-PseTNC-General', 'PC-PseAAC', 'PC-PseAAC-General']
-theta_2_methods = ['SC-PseDNC-General', 'SC-PseTNC-General', 'SC-PseAAC', 'SC-PseAAC-General']
+K_2_DNA_METHODS = ['PseDNC', 'PC-PseDNC-General', 'SC-PseDNC-General', 'DAC', 'DCC', 'DACC']
+K_3_DNA_METHODS = ['PC-PseTNC-General', 'SC-PseTNC-General', 'TAC', 'TCC', 'TACC']
 
-di_inds_6_DNA = ['Rise', 'Roll', 'Shift', 'Slide', 'Tilt', 'Twist']
-tri_inds_DNA = ['Dnase I', 'Bendability (DNAse)', 'Bendability (consensus)', 'Trinucleotide GC Content',
+THETA_1_METHODS = ['PseDNC', 'PC-PseDNC-General', 'PC-PseTNC-General', 'PC-PseAAC', 'PC-PseAAC-General']
+THETA_2_METHODS = ['SC-PseDNC-General', 'SC-PseTNC-General', 'SC-PseAAC', 'SC-PseAAC-General']
+
+DI_INDS_6_DNA = ['Rise', 'Roll', 'Shift', 'Slide', 'Tilt', 'Twist']
+TRI_INDS_DNA = ['Dnase I', 'Bendability (DNAse)', 'Bendability (consensus)', 'Trinucleotide GC Content',
                 'Nucleosome positioning', 'Consensus_roll', 'Consensus-Rigid', 'Dnase I-Rigid', 'MW-Daltons',
                 'MW-kg', 'Nucleosome', 'Nucleosome-Rigid']
 
-di_inds_RNA = ['Twist (RNA)', 'Tilt (RNA)', 'Roll (RNA)', 'Rise (RNA)', 'Slide (RNA)', 'Shift (RNA)',
-                'Stacking energy (RNA)', 'Enthalpy (RNA)1', 'Entropy (RNA)', 'Free energy (RNA)', 'Hydrophilicity (RNA)']
-inds_3_Protein = ['Hydrophobicity', 'Hydrophilicity', 'Mass']
+DI_INDS_RNA = ['Twist (RNA)', 'Tilt (RNA)', 'Roll (RNA)', 'Rise (RNA)', 'Slide (RNA)', 'Shift (RNA)',
+               'Stacking energy (RNA)', 'Enthalpy (RNA)1', 'Entropy (RNA)', 'Free energy (RNA)', 'Hydrophilicity (RNA)']
+INDS_3_PROTEIN = ['Hydrophobicity', 'Hydrophilicity', 'Mass']
